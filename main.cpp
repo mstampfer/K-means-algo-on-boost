ef111108b73ef401f3a0f78533e30e7525dc625c
mstampfer <github@axonconsulting.com>
2015-03-18 14:37:33 +0000

    commiting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <map>
 #include <string>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
@@ -10,34 +9,39 @@ auto sim_distance(const map<const string, map<const string, float>>& prefs,
                   const string& person2)
 {
 // Get the list of shared_items
-   map<string, float> si;
-   for(const auto & item1 : prefs.at(person1) ) {
-      for (const auto &item2 : prefs.at(person2)) {
-         if (item1 == item2)
-            si[item1.first] = 1.0;
-      }
-   }
-//      if (auto it =
-//              find_if(prefs.cbegin(), prefs.cend(), [item, person2](const map<const string, float>& val)
-//               {
-//                  return val.at(person2) == item;
-//               }) != prefs.cend())
-//      si[item] = 1;
+//   for(const auto & item1 : prefs.at(person1) ) {
+//      for (const auto &item2 : prefs.at(person2)) {
+//         if (item1.first == item2.first)
+//            si[item1.first] = 1.0;
+//      }
 //   }
+    map<string, float> si;
+    auto person2_items = prefs.at(person2);
+    for(const auto & person1_item : prefs.at(person1) ) {
+        for_each(person2_items.cbegin(), person2_items.cend(), [&person1_item, &si](auto &person2_item) {
+            if (person1_item.first == person2_item.first)
+                si[person1_item.first] = 1.0;
+        });
+    }
+
+
 
    // if they have no ratings in common, return 0
       if (si.size()==0)
-         return 0;
+         return 0.0;
+    else
+          cout << si.size();
+
    // Add up the squares of all the differences
       auto sum_of_squares=0;//sum([pow(prefs[person1][item]-prefs[person2][item],2)
                           //for item in prefs[person1] if item in prefs[person2]])
       return 1.0/(1.0+sum_of_squares);
 }
 
-int main() {
+void main() {
 
 
-   map<const string, map<const string, float>> critics = {
+   const map<const string, map<const string, float>> critics = {
            {"Lisa Rose", {{"Lady in the Water", 2.5}, {"Snakes on a Plane", 3.5},
                    {"Just My Luck", 3.0}, {"Superman Returns", 3.5}, {"You, Me and Dupree", 2.5},
                    {"The Night Listener", 3.0}}},
@@ -56,10 +60,7 @@ int main() {
                    {"The Night Listener", 3.0}, {"Superman Returns", 5.0}, {"You, Me and Dupree", 3.5}}},
            {"Toby", {{"Snakes on a Plane", 4.5}, {"You, Me and Dupree", 1.0}, {"Superman Returns", 4.0}}}
    };
-   auto xx = critics["Lisa Rose"]["Lady in the Water"];
-   cout << xx << endl;
-   critics["Toby"]["Snakes on a Plane"]=3.5;
-   cout << critics["Toby"]["Snakes on a Plane"] << endl;
+    sim_distance(critics, "Lisa Rose", "Gene Seymour");
 }
 
 
