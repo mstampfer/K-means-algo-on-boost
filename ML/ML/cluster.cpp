4c9e98d988149ec9220f322d5fc81bbf494bd377
mstampfer <github@axonconsulting.com>
2015-03-28 14:43:55 +0000

    bug fixeasi: erase-remove idiom, printing cluster tree

--- ML/ML/cluster.cpp
+++ ML/ML/cluster.cpp
@@ -75,31 +75,31 @@ auto Bicluster::hcluster(const vector<vector<double>>& rows,
 {
 	vector<double> mergevec;
 	vector<Bicluster> clusters;
-
 	set<unsigned, std::greater<int>> lowestpair;
 	vector<unsigned> lowestpairv;
 	int currentclustid = -1;
-
-	map<set<unsigned>, double> distances;
 	int cid = 0;
+
 	//	Clusters are initially just the rows
 	for (const auto& row : rows)
 	{
 		//Bicluster clust(++cid, row);
-		clusters.emplace_back(++cid, row);
+		clusters.emplace_back(cid++, row);
 	}
 
 	while (clusters.size() > 1)
 	{
 		lowestpair = { 1, 0 };
 		lowestpairv = { 1, 0 };
+		map<set<unsigned>, double> distances;
 		auto closest = distance(clusters[0].vec, clusters[1].vec);
+	
 		// loop through every pair looking for the smallest distance
 		for (unsigned i = 1; i < clusters.size(); ++i)
 		{
 			for (unsigned j = i + 1; j < clusters.size(); ++j)
 			{
-				cout << "i " << i << " j " << j << " " << clusters.size() << endl;
+				//cout << "i " << i << " j " << j << " " << clusters.size() << endl;
 				// distances is the cache of distance calculations
 				set<unsigned> test{ i, j };
 				if (distances.find(test) == distances.cend())
@@ -112,59 +112,59 @@ auto Bicluster::hcluster(const vector<vector<double>>& rows,
 					lowestpairv = { i, j };
 				}
 			}
-			// calculate the average of the two clusters
-			if (lowestpair.size() > 0)
+		}
+		// calculate the average of the two clusters
+		if (lowestpair.size() > 0)
+		{
+			for (unsigned idx = 0; idx < clusters[0].vec.size(); ++idx)
 			{
-				for (unsigned idx = 0; idx < clusters[0].vec.size(); ++idx)
-				{
-					mergevec.push_back((clusters[lowestpairv[0]].vec[idx]
-						+ clusters[lowestpairv[1]].vec[idx]) / 2.0);
-				}
-
-				//create the new cluster
-				clusters.emplace_back(currentclustid,
-					mergevec,
-					make_unique<Bicluster>(move(clusters[lowestpairv[0]])),
-					make_unique<Bicluster>(move(clusters[lowestpairv[1]])),
-					closest);
-
-				//remove merged clusters
-				for (const auto& e : lowestpair)
-					clusters.erase(clusters.begin() + e);
-
-				lowestpair = {};
-
-				//cluster ids that weren't in the original set are negative
-				currentclustid -= 1;
+				mergevec.push_back((clusters[lowestpairv[0]].vec[idx]
+					+ clusters[lowestpairv[1]].vec[idx]) / 2.0);
 			}
+
+			//create the new cluster
+			clusters.emplace_back(currentclustid,
+				mergevec,
+				make_unique<Bicluster>(move(clusters[lowestpairv[0]])),
+				make_unique<Bicluster>(move(clusters[lowestpairv[1]])),
+				closest);
+
+			//remove merged clusters
+			for (const auto& e : lowestpair)
+			{
+				cout << e << " ";
+				clusters.erase(clusters.begin() + e);
+			}
+			cout << lowestpair.size() << endl;
+			lowestpair = {};
+
+			//cluster ids that weren't in the original set are negative
+			currentclustid -= 1;
 		}
 	}
 	return move(clusters[0]);
 }
 
-void Bicluster::printclust(const Bicluster& cluster, const vector<string>& labels, unsigned n = 3) 
+void Bicluster::printclust(const Bicluster& cluster, const vector<string>& labels, unsigned n = 0) 
 {
 	// indent to make a hierarchy layout
-	for (unsigned i = 0; i < n; ++i)
+	for (unsigned ws = 0; ws < n; ++ws) cout << ' ';
+	if (cluster.id < 0)
+		// negative id means that this is branch
+		cout << '-' << endl;
+	else
 	{
-		for (unsigned ws = 0; ws < n; ++ws) cout << ' ';
-		if (cluster.id < 0)
-			// negative id means that this is branch
-			cout << '-' << endl;
+		// positive id means that this is an endpoint
+		if (labels.size() == 0)
+			cout << cluster.id << endl;
 		else
-		{
-			// positive id means that this is an endpoint
-			if (labels.size() == 0)
-				cout << cluster.id << endl;
-			else
-				cout << labels[cluster.id] << endl;
-		}
-		 //now print the right and left branches
-		if (cluster.left.get())
-			printclust(*cluster.left, labels, n + 1);
-		if (cluster.right.get())
-			printclust(*cluster.right, labels, n + 1);
+			cout << labels[cluster.id] << endl;
 	}
+		//now print the right and left branches
+	if (cluster.left.get())
+		printclust(*cluster.left, labels, n + 1);
+	if (cluster.right.get())
+		printclust(*cluster.right, labels, n + 1);
 }
 int main()
 {
